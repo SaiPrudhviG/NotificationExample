8f6b45fb7a4bccd499890b5dc63145115885e605
SaiPrudhviG <38295435+SaiPrudhviG@users.noreply.github.com>
2019-02-23 03:34:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prudhvi/cameraintent/NotificationView.java
@@ -0,0 +1,18 @@
+package com.example.prudhvi.cameraintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NotificationView extends Activity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_view);
+        textView = findViewById(R.id.textView);
+        //getting the notification message
+        String message=getIntent().getStringExtra("message");
+        textView.setText(message);
+    }
+}
